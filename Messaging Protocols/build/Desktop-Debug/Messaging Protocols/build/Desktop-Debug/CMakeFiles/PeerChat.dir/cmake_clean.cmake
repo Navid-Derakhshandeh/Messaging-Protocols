a0d346e90fc8b278f53f71aba3e2d2a9a1edb81e
Navid Derakhshandeh <navid.derakhshandeh1@gmail.com>
2025-10-26 15:26:20 +0330

    Add files via upload

---
+++ Messaging Protocols/build/Desktop-Debug/Messaging Protocols/build/Desktop-Debug/CMakeFiles/PeerChat.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PeerChat.dir/link.d"
+  "CMakeFiles/PeerChat_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/PeerChat_autogen.dir/ParseCache.txt"
+  "PeerChat_autogen"
+  "CMakeFiles/PeerChat.dir/PeerChat_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/PeerChat.dir/PeerChat_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/PeerChat.dir/main.cpp.o"
+  "CMakeFiles/PeerChat.dir/main.cpp.o.d"
+  "CMakeFiles/PeerChat.dir/mainwindow.cpp.o"
+  "CMakeFiles/PeerChat.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/PeerChat.dir/peerconnection.cpp.o"
+  "CMakeFiles/PeerChat.dir/peerconnection.cpp.o.d"
+  "CMakeFiles/PeerChat.dir/server.cpp.o"
+  "CMakeFiles/PeerChat.dir/server.cpp.o.d"
+  "PeerChat"
+  "PeerChat.pdb"
+  "PeerChat_autogen/mocs_compilation.cpp"
+  "PeerChat_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PeerChat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Messaging Protocols/build/Desktop-Debug/Messaging Protocols/build/Desktop-Debug/CMakeFiles/Protocols_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Protocols_autogen"
+  "Protocols_autogen/include/ui_mainwindow.h"
+  "Protocols_autogen/mocs_compilation.cpp"
+  "Protocols_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Protocols_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
