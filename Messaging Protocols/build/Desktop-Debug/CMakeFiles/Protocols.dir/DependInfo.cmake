082106b42d5bf702cfe1e455112d547f3a888ee0
Navid Derakhshandeh <navid.derakhshandeh1@gmail.com>
2025-10-26 15:35:40 +0330

    Add files via upload

---
+++ Messaging Protocols/build/Desktop-Debug/CMakeFiles/Protocols.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "Protocols_autogen/timestamp" "custom" "Protocols_autogen/deps"
+  "/home/navid/PeerChat/build/Desktop-Debug/Protocols_autogen/mocs_compilation.cpp" "CMakeFiles/Protocols.dir/Protocols_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/Protocols.dir/Protocols_autogen/mocs_compilation.cpp.o.d"
+  "/home/navid/Protocols/main.cpp" "CMakeFiles/Protocols.dir/main.cpp.o" "gcc" "CMakeFiles/Protocols.dir/main.cpp.o.d"
+  "/home/navid/Protocols/mainwindow.cpp" "CMakeFiles/Protocols.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/Protocols.dir/mainwindow.cpp.o.d"
+  "/home/navid/Protocols/peerconnection.cpp" "CMakeFiles/Protocols.dir/peerconnection.cpp.o" "gcc" "CMakeFiles/Protocols.dir/peerconnection.cpp.o.d"
+  "/home/navid/Protocols/server.cpp" "CMakeFiles/Protocols.dir/server.cpp.o" "gcc" "CMakeFiles/Protocols.dir/server.cpp.o.d"
+  "" "Protocols" "gcc" "CMakeFiles/Protocols.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
